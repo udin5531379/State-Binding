01667d63bc07d85124cfe97db1baee81801f65f4
System Administrator <root@MacBook-Pro.local>
2021-02-09 05:03:35 -0600

    first Commit

---
+++ State&Binding/State_BindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  State_BindingApp.swift
+//  State&Binding
+//
+//  Created by Udin Rajkarnikar on 2/9/21.
+//
+
+import SwiftUI
+
+@main
+struct State_BindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
